bd0d927b4d198a8844acb38a52bea394d8ef0eae
miker-bice <mikejerrson.galindez@gmail.com>
2022-05-18 11:30:34 +0800

    base html done | home html done

--- stockApp/urls.py
+++ stockApp/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "stockApp"
 
 urlpatterns = [
-
+    path('', views.home, name='home'),
 ]
--- stockApp/views.py
+++ stockApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# this is for the home views
+def home(request):
+    return render(request, 'stockApp/home.html', {})
