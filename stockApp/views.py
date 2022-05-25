19fe7dee6d51b32f39f3952cf78b3738eff49939
miker-bice <mikejerrson.galindez@gmail.com>
2022-05-25 12:23:40 +0800

    major change

--- stockApp/views.py
+++ stockApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 # imports for stocks forecasting and graph plotting
 import yfinance as yf
 import datetime as dt
@@ -19,7 +20,7 @@ def home(request):
     prev_lastquote = data['Close'].iloc[-2]
     degree = "(" + str(round(((last_quote - prev_lastquote) / abs(last_quote)) * 100, 2)) + "%)"
     change_val = round(last_quote - prev_lastquote, 2)
-    change_pct= str(round(last_quote-prev_lastquote,2))+degree
+    change_pct = str(round(last_quote-prev_lastquote,2))+degree
     context = {
         'current_price': round(last_quote, 3),
         'change_pct': change_pct,
@@ -30,115 +31,123 @@ def home(request):
 
 
 def update_stock(request):
-    # change this to change the data source of stock from yahoo finance
-    SOURCE = 'PSEI.PS'
-    stock_ticker = yf.Ticker('PSEI.PS')
-    stock_company = stock_ticker.info['shortName']
+    # this accepts the form fields
+    simulation_days = int(request.POST['simulation_days'])
+    simulation_number = int(request.POST['simulation_number'])
 
-    # code to get the data from yahoo finance using yfinance API, supplying the stock name, start date, and end date
-    # datetime(year,month,day)
-    start = dt.datetime(2021, 5, 19)
-    end = dt.datetime(2022, 5, 19)
-    stock_data = yf.download(SOURCE, start, end)
+    if simulation_days == 0 or simulation_number == 0:
+        messages.error(request, f'Invalid number of Days or number of Simulation')
+        return redirect('stockApp:home')
+    else:
+        # change this to change the data source of stock from yahoo finance
+        SOURCE = 'PSEI.PS'
+        stock_ticker = yf.Ticker('PSEI.PS')
+        stock_company = stock_ticker.info['shortName']
 
-    # we get stock returns by comparing the percent change of current day price to the previous day price of..
-    # .. adjusted close prices
-    stock_returns = stock_data['Adj Close'].pct_change()
-    # standard deviation of the stock returns
-    daily_volatility = stock_returns.std()
+        # code to get the data from yahoo finance using yfinance API, supplying the stock name, start date, and end date
+        # datetime(year,month,day)
+        start = dt.datetime(2021, 5, 19)
+        end = dt.datetime(2022, 5, 19)
+        stock_data = yf.download(SOURCE, start, end)
 
-    # setting of initial variables needed
-    trading_days = 10
-    last_prices = []  # last price array
-    last_price = stock_data['Adj Close'][-1]  # set initial value of the last price
+        # we get stock returns by comparing the percent change of current day price to the previous day price of..
+        # .. adjusted close prices
+        stock_returns = stock_data['Adj Close'].pct_change()
+        # standard deviation of the stock returns
+        daily_volatility = stock_returns.std()
 
-    TOTAL_SIMULATIONS = 1000
-    # df = pd.DataFrame()
-    temp_frames = []
-    for x in range(TOTAL_SIMULATIONS):
-        counter = 0
-        prices = []
-        # we get the simulated price by drawing random samples from a normal distribution using numpy where..
-        # .. mean =0 , standard deviation = daily_volatility, ..
-        # .. and multiplying the sample with the last price on the list.
-        price = last_price * (1 + np.random.normal(0, daily_volatility))
-        prices.append(price)
+        # setting of initial variables needed
+        trading_days = simulation_days
+        last_prices = []  # last price array
+        last_price = stock_data['Adj Close'][-1]  # set initial value of the last price
 
-        for y in range(trading_days):
-            if counter > trading_days - 1:
-                break
-            # this code also gets the simulated price using the same method but multiplies the random normal
-            # distribution sample with the elements on the prices array
-            price = prices[counter] * (1 + np.random.normal(0, daily_volatility))
+        TOTAL_SIMULATIONS = simulation_number
+        # df = pd.DataFrame()
+        temp_frames = []
+        for x in range(TOTAL_SIMULATIONS):
+            counter = 0
+            prices = []
+            # we get the simulated price by drawing random samples from a normal distribution using numpy where..
+            # .. mean =0 , standard deviation = daily_volatility, ..
+            # .. and multiplying the sample with the last price on the list.
+            price = last_price * (1 + np.random.normal(0, daily_volatility))
             prices.append(price)
-            counter += 1
 
-        # df[x] = prices
-        temp_frame = pd.DataFrame()
-        temp_frame[x] = prices
-        temp_frames.append(temp_frame)
-        last_prices.append(prices[-1])
-    df = pd.concat(temp_frames,axis=1)
-    # this is used for setting the plot
-    plt.style.use('ggplot')
-    title = str(TOTAL_SIMULATIONS) + " Monte Carlo simulations of :\n" + str(stock_company) + " for " + str(
-        trading_days) + " days"
-    fig1, ax1 = plt.subplots()
-    ax1.plot(df)
-    ax1.set_title(title)
-    ax1.set_ylabel("Price")
-    ax1.set_xlabel("Days")
+            for y in range(trading_days):
+                if counter > trading_days - 1:
+                    break
+                # this code also gets the simulated price using the same method but multiplies the random normal
+                # distribution sample with the elements on the prices array
+                price = prices[counter] * (1 + np.random.normal(0, daily_volatility))
+                prices.append(price)
+                counter += 1
+
+            # df[x] = prices
+            temp_frame = pd.DataFrame()
+            temp_frame[x] = prices
+            temp_frames.append(temp_frame)
+            last_prices.append(prices[-1])
+        df = pd.concat(temp_frames,axis=1)
+        # this is used for setting the plot
+        plt.style.use('ggplot')
+        title = str(TOTAL_SIMULATIONS) + " Monte Carlo simulations of :\n" + str(stock_company) + " for " + str(
+            trading_days) + " days"
+        fig1, ax1 = plt.subplots()
+        ax1.plot(df)
+        ax1.set_title(title)
+        ax1.set_ylabel("Price")
+        ax1.set_xlabel("Days")
 
 
-    tickers = [SOURCE]
-    for ticker in tickers:
-        ticker_yahoo = yf.Ticker(ticker)
-        data = ticker_yahoo.history()
-        last_quote = data['Close'].iloc[-1]
+        tickers = [SOURCE]
+        for ticker in tickers:
+            ticker_yahoo = yf.Ticker(ticker)
+            data = ticker_yahoo.history()
+            last_quote = data['Close'].iloc[-1]
 
-    plt.axhline(y=last_quote, color='black', linestyle='-')
-    # fig = plt.figure()
-    # title = str(TOTAL_SIMULATIONS) + " Monte Carlo simulations of :\n" + str(stock_company) + " for " + str(
-    #     trading_days) + " days"
-    # fig.suptitle(title)
-    # fig.set_figwidth(8)
-    # fig.set_figheight(5)
-    # plt.plot(df)
-    # plt.xlabel('Days')
-    # plt.ylabel('Price')
-    # filename = str(TOTAL_SIMULATIONS) + "-" + str(trading_days) + ".png"
-    current_directory = "./static/images/graph"
-    filename = "forecast_graph.png"
-    plt.savefig(os.path.join(current_directory, filename), dpi=300)
-    # plt.show()
+        plt.axhline(y=last_quote, color='black', linestyle='-')
+        # fig = plt.figure()
+        # title = str(TOTAL_SIMULATIONS) + " Monte Carlo simulations of :\n" + str(stock_company) + " for " + str(
+        #     trading_days) + " days"
+        # fig.suptitle(title)
+        # fig.set_figwidth(8)
+        # fig.set_figheight(5)
+        # plt.plot(df)
+        # plt.xlabel('Days')
+        # plt.ylabel('Price')
+        # filename = str(TOTAL_SIMULATIONS) + "-" + str(trading_days) + ".png"
+        current_directory = "./static/images/graph"
+        filename = "forecast_graph.png"
+        plt.savefig(os.path.join(current_directory, filename), dpi=300)
+        # plt.show()
 
-    expected_price = np.mean(last_prices)
-    quantile_five = np.percentile(last_prices, 5)
-    quantile_ninetyfive = np.percentile(last_prices, 95)
+        expected_price = np.mean(last_prices)
+        quantile_five = np.percentile(last_prices, 5)
+        quantile_ninetyfive = np.percentile(last_prices, 95)
 
-    print("Current price: ", last_quote)
-    print("Expected price: ", expected_price)
-    print("Quantile (5%): ", quantile_five)
-    print("Quantile (95%): ", quantile_ninetyfive)
+        print("Current price: ", last_quote)
+        print("Expected price: ", expected_price)
+        print("Quantile (5%): ", quantile_five)
+        print("Quantile (95%): ", quantile_ninetyfive)
 
-    # this is to plot the second graph (histogram)
+        # this is to plot the second graph (histogram)
 
-    # fig2, ax2 = plt.subplots()
-    # ax2.hist(last_prices, bins=100, color='#6397ff')
-    # ax2.set_title(title)
-    # ax2.set_ylabel("Price")
-    # ax2.set_xlabel("Days")
-    # plt.axvline(np.percentile(last_prices, 5), color='r', linestyle='dashed', linewidth=2)
-    # plt.axvline(np.percentile(last_prices, 95), color='r', linestyle='dashed', linewidth=2)
-    #
-    # filename = "histogram.png"
-    # plt.savefig(os.path.join(current_directory, filename), dpi=300)
-    # plt.show()
+        # fig2, ax2 = plt.subplots()
+        # ax2.hist(last_prices, bins=100, color='#6397ff')
+        # ax2.set_title(title)
+        # ax2.set_ylabel("Price")
+        # ax2.set_xlabel("Days")
+        # plt.axvline(np.percentile(last_prices, 5), color='r', linestyle='dashed', linewidth=2)
+        # plt.axvline(np.percentile(last_prices, 95), color='r', linestyle='dashed', linewidth=2)
+        #
+        # filename = "histogram.png"
+        # plt.savefig(os.path.join(current_directory, filename), dpi=300)
+        # plt.show()
 
-    # this is the session that will save the data form this function
-    # del request.session['current_price']
-    # request.session['current_price'] = round(last_quote, 2)
+        # this is the session that will save the data form this function
+        # del request.session['current_price']
+        # request.session['current_price'] = round(last_quote, 2)
 
-    # in this return, redirect the users to the home template
-    return redirect('stockApp:home')
+        # in this return, redirect the users to the home template
+        return redirect('stockApp:home')
     
